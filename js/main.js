43fc6757bdd25a823bea524ea37b2c127537edb2
jiboo01 <130724366+jiboo01@users.noreply.github.com>
2023-08-28 13:00:01 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
+// Services section - Modal
+
+// Portfolio section - Modal
+
+// Out clients - Swiper
+
+// Website dark/light theme
+
+// Scroll to top button
+
+// Navigation menu items active on page scroll
+
+// Responsive navigation menu toggle
+
+// Scroll reveal animations
+// Common reveal options to create reveal animations
+
+// Target elements, and specify options to create reveal animations
